04df5984a487a3b68909325e9f661bf30a25c6ca
Shyan Barker <32976428+bazookabubblegum@users.noreply.github.com>
2018-04-04 20:10:20 -0700

    commit3
    some styling, still difficulty with function

--- zodiac/javascript/style.js
+++ zodiac/javascript/style.js
@@ -1,149 +1,158 @@
 // array of objects to store zodiac data
 var zodiac = [
 	{
-		image1: "",
+		image1: "img/aquarius2.jpg",
 		sign: "aquarius",
 		obstacles: "",
-		fortune: "Knowledge, Humanitarian, Serious, Insightful, Duplicitous",
-		ruling planet: "",
+		fortune: "",
+		rulingplanet: "",
 		companions: "",
-		image: "images/aquarius.png",
+		elemental: "",
+		image: "img/aquarius.jpg",
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/aries2.jpg",
 		sign: "aries",
-		obstacles: "",
-		fortune: "Active, Demanding, Determined, Effective, Ambitious",
-		ruling planet: "",
+		obstacles: "You, yourself, can be challenging, and guilt is not too far behind for what has not been accomplished. Let things go, and remember you are constantly changing. You burn out fast, as your element suggests you may also burn out others. Keep the peace by letting go of rigidity. You may be on the mountain, but you are designed of different particle patterns.",
+		fortune: "You are ambitious and impulsive. Intention sees no bounds within you. Each day will bring you closer to the goals you wish to conquer. As a Ram, you attack things head on, but you can sidestep solutions even on the steepest mountain tops.",
+		rulingplanet: "Mars",
 		companions: "",
-		image: "images/aries.png"
+		elemental: "Fire",
+		image: "img/aries.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/cancerAMRITBAR.jpg",
 		sign: "cancer",
 		obstacles: "",
-		fortune: "Emotion, Diplomatic, Intensity, Impulsive, Selective",
-		ruling planet: "",
+		fortune: "",
+		rulingplanet: "",
 		companions: "",
-		image: "images/cancer.png"
+		elemental: "",
+		image: "img/cancer.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/capricornBLACKBIRD.jpg",
 		sign: "capricorn",
 		obstacles: "",
-		fortune: "Determination, Dominance, Perservering, Practical, Willful",
-		ruling planet: "",
+		fortune: "",
+		rulingplanet: "",
 		companions: "",
-		image: "images/capricorn.png"
+		elemental: "",
+		image: "img/capricornMARDELRUBIO.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/gemini1.jpg",
 		sign: "gemini",
 		obstacles: "",
-		fortune: "Communication, Indecision, Inquisitive, Intelligent, Changeable",
-		ruling planet: "",
+		fortune: "",
+		rulingplanet: "",
 		companions: "",
-		image: "images/gemini.png"
+		elemental: "",
+		image: "img/gemini.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/leo3.jpg",
 		sign: "leo",
 		obstacles: "",
-		fortune: "Ruling, Warmth, Generosity, Faithful, Initiative",
-		ruling planet: "",
+		fortune: "",
+		rulingplanet: "",
 		companions: "",
-		image: "images/leo.png"
+		elemental: "",
+		image: "img/leo.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/libra1.jpg",
 		sign: "libra",
 		obstacles: "",
-		fortune: "Balance, Justice, Truth, Beauty, Perfection",
-		ruling planet: "",
+		fortune: "",
+		rulingplanet: "",
 		companions: "",
-		image: "images/libra.png"
+		elemental: "",
+		image: "img/libra2.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/pisces1.jpg",
 		sign: "pisces",
 		obstacles: "",
-		fortune: "Fluctuation, Depth, Imagination, Reactive, Indecisive",
-		ruling planet: "",
+		fortune: "",
+		rulingplanet: "",
 		companions: "",
-		image: "images/pisces.png"
+		elemental: "",
+		image: "img/pisces2.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/sagitarus.jpg",
 		sign: "sagittarius",
 		obstacles: "",
-		fortune: "Philosophical, Motion, Experimentation, Optimism",
+		fortune: "",
 		rulingplanet: "",
 		companions: "",
-		image: "images/sagittarius.png"
+		elemental: "",
+		image: "img/sagitarius1.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/scorpioBIRDBLACK.jpg",
 		sign: "scorpio",
 		obstacles: "",
-		fortune: "Transient, Self-Willed, Purposeful, Unyielding",
+		fortune: "",
 		rulingplanet: "",
 		companions: "",
-		image: "images/scorpio.png"
+		elemental: "",
+		image: "img/scorpio.jpg"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/taurus1.jpg",
 		sign: "taurus",
 		obstacles: "",
-		fortune: "	Security, Subtle strength, Appreciation, Instruction, Patience",
-		rulingplanet: "",
+		fortune: "",
+		rulingplanet: "Venus",
 		companions: "",
-		image: "images/taurus.png"
+		elemental: "",
+		image: "img/taurus2.png"
+		dates: ""
 	},
 	{
-		image1: "",
+		image1: "img/virgo2.jpg",
 		sign: "virgo",
 		obstacles:
-		fortune: "Analyzing, Practical, Reflective, Observation, Thoughtful",
+		fortune: "",
 		rulingplanet: "",
 		companions: "",
-		image: "img/virgo2.jpg"
+		elemental: "",
+		image: "img/virgo.jpg"
+		dates: ""
 	}
 ];
-// This is some sweet code so the enter key also implements the function
-var btn = document.getElementById('userdata');
-btn.addEventListener('keypress', function enterKey(e) {
-	if(e.keyCode == 13) {
-		horoscope();
-	};
-}, false);
-
-// function to determine user horoscope, function fires on user clicking button line 24 index.html
+//should run through zodiac array
 function horoscope() {
 	// store the tag with id="sign" in var userdata
 	var userdata = document.getElementById("userdata");
 
+// function to determine user horoscope
+	var userdata = document.getElementById("yourSign");
 
 	// loop through 
 	for(var i = 0; i < zodiac.length; i = i + 1) {
-		//capitilization doesnt matter
-		console.log("users value lowercase is: " + userdata.value.toLowerCase());
-		// check the current item's sign in the zodiac array
-		console.log("current zodiac sign in loop is: " + zodiac[i].sign);
 
-		// if the value the user typed in, changed to all lowercase letters, is equal to one of our signs, then we do something
+		// if the value the user typed in is lowercase letters
 		if(userdata.value.toLowerCase() === zodiac[i].sign) {
-			
-
-			console.log("users typed in: " + userdata.value);
-			console.log("current image value is: " + zodiac[i].image);
-			console.log("current fortune value is: " + zodiac[i].fortune);
 
 			// get element with id="userSign" and change the text to the user input
 			document.getElementById("yourSign").textContent = userdata.value;
 			// get element with id="icon" and change source attribute to current zodiac image path
 			document.getElementById("icon").src = zodiac[i].image;
 
-			// // get element with id="yourHoroscope" and change the content to this concatenated string
+			// get element with id="yourHoroscope" and change the content to this concatenated string
 			document.getElementById("yourHoroscope").textContent = "Your fortune is: " + zodiac[i].fortune;
 
 			document.getElementById("yourHoroscope").textContent = "Your ruling planet is: " + zodiac[i].rulingplanet;
@@ -151,16 +160,13 @@ function horoscope() {
 			document.getElementById("yourHoroscope").textContent = "Your obstacles include: " + zodiac[i].obstacles;
 
 			document.getElementById("yourHoroscope").textContent = "Your harmonizing zodiac companions are: " + zodiac[i].companion;
+
+			document.getElementById("yourHoroscope").textContent = "Your elemental balance is: " + zodiac[i].elemental;
+
+			document.getElementById("yourHoroscope").textContent = "Dates of this Horoscope: " + zodiac[i].dates;
+
 			// stop the function because we found a match and added the data to the screen!!!
 			return;
 		};
-
-		// confirm no matches are found if that's the case
-		console.log("no matches were found, user failed to type in a correct zodiac sign");
-
-		// if no match is found, do opposite of above, mostly clearing content and images
-		document.getElementById("yourSign").textContent = "This is not a Zodiac Sign, Please try again.";
-		document.getElementById("yourHoroscope").textContent = "";
-		document.getElementById("icon").src = "";
 	};
 };
